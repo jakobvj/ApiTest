dbb47e15d4841830405a0fd3e3c7a01aec3b1514
Jakob Jensen <jvj@eucsyd.dk>
2019-04-01 14:58:07 +0200

    Called repository from controller to get camps

--- ApiTest/Controllers/CampsController.cs
+++ ApiTest/Controllers/CampsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiTest.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +11,25 @@ namespace ApiTest.Controllers
     [Route("api/[controller]")]
     public class CampsController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetCamps()
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
         {
-            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp" });
+            _repository = repository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCamps()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+            
         }
     }
 }
